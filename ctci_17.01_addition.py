d58c9f65ba86dec87ab6641ebd313b6a4a0b3012
Eric Westman <haldous2@gmail.com>
2021-09-07 14:24:26 -0700

    Update ctci_17.01_addition.py

--- ctci_17.01_addition.py
+++ ctci_17.01_addition.py
@@ -6,54 +6,66 @@ Will have to create manual addition of bits
 
 Assumptions: integers only - might modify version 2 for floats?
 """
-def add(a, b):
+def int_overflow(val):
     """
-    Addition breaks when adding and subracting same value 
-       e.g. (2 + -2)
-       It looks like the carry grows exponentially 
-       because there isn't anything to carry. 
-       same issue with subtraction below
-       The only fix I can think of is to check if numbers are 
-       equal and opposite before adding them
+    Overflow handler
+    Because python doesn't have internal types, this method will
+    emulate strongly typed integer overflow
+    e.g., if min is -6 and max is 5
+       val: -3, -2, -1, 0, 1, 2, 3, 4, 5,  6,  7,  8
+       ret: -3, -2, -1, 0, 1, 2, 3, 4, 5, -6, -5, -4 ... (repeats)
     """
-    i = 0
+    if not -sys.maxint-1 <= val <= sys.maxint:
+        val = (val + (sys.maxint + 1)) % (2 * (sys.maxint + 1)) - (sys.maxint + 1)
+    return val
+
+def addbin(a, b):
+    """
+    Add binary a and b
+    It looks like the carry grows exponentially 
+       because there isn't anything to carry.
+    Fix: because python doesn't have strong type, integers tend to 
+         meander off into infinity. The fix is to implement a manual
+         overflow method that cuts values off as though they were 32 bit ints
+    """
     while b != 0:
-        if i == 5:
-            break
-        i += 1
-        r = a ^ b           # result
-        c = (a & b) << 1    # carry
-        print a, b, r, c
-        a = r
-        b = c
-    print "=== result", a
+        r = a ^ b               # add without carry
+        c = (a & b) << 1        # carry
+        a = int_overflow(r)
+        b = int_overflow(c)
     return a
 
-add(2, 3)
-add(12, 5)
-add(3, -3)    # crash
+def add(a, b):
+    print "result add", a, "and", b, "=", addbin(a, b)
+
+add(2, 3)       # 5
+add(15, 9)      # 24
+add(2, -1)      # 1
+add(2, -2)      # 0
+add(2, -10)     # -8
 
 # def subtract(a, b):
 #     return add(a, b * -1)
 
-def subtract(a, b):
+def subtractbin(a, b):
     """
-    Subtraction breaks if denom is larger than numerator
-    Way to solve: 
-       1. swap denominator and numerator, flip sign
-       2. add to negative number - add(a, -b)
+    Subtract binary b from a
+    It looks like the borrow grows exponentially 
+       because there isn't anything to borrow.
+    Fix: because python doesn't have strong type, integers tend to 
+         meander off into infinity. The fix is to implement a manual
+         overflow method that cuts values off as though they were 32 bit ints
     """            
-    i = 0
     while b != 0:
-        if i == 10:
-            break
-        i += 1
+        r = a ^ b               # add without borrow
         c = ((~a) & b) << 1     # borrow
-        a = a ^ b               # add without borrow
-        b = c
-        print a, b, c
-    print "=== result", a
+        a = int_overflow(r)
+        b = int_overflow(c)
+    return a
 
-subtract(2, 5)
-subtract(5, 2)  # crash
-subtract(2, 2)
+def subtract(a, b):
+    print "result subtract", b, "from", a, "=", subtractbin(a, b)
+
+subtract(2, 5)      # -3
+subtract(5, 2)      # 3
+subtract(2, 2)      # 0
